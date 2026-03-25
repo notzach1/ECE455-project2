1b09bbcd557bbf9eb1b79038e7550ed3fc1a321f
notzach1 <a.zach2002@gmail.com>
2026-03-24 19:29:50 -0700

    Update main.c

--- main.c
+++ main.c
@@ -57,35 +57,144 @@ typedef struct{
 
 ////////////////////////////////
 //Queue handles 
-QueueHandle_t dds_que; //que for generator, user, and monitor to send a message to the dds
+QueueHandle_t dd_scheduler_que; //que for generator, user, and monitor to send a message to the dds
 QueueHandle_t task_missed_que; //dds to task gen task missed its deadline needs to be released again by gen
 
 
 ////////////////////////////////
 //LISTs
-static dd_task_list *scheduled_task_list;
-static dd_task_list *done_task_list;
+static dd_task_list *active_task_list;
+static dd_task_list *complete_task_list;
 static dd_task_list *task_missed_list;
 
 //*************timeout
 
 //functions 
-void dds_f_task (void *pv);
+void dd_scheduler_task (void *pv);
 void task_generator (void *pv);
 void monitor_task (void *pv);
+void t1_user (void *pv);
+void t2_user (void *pv);
+void t3_user (void *pv);
+
 //user tasks
-void user_task_1(void *pv);
-void user_task_2(void *pv);
-void user_task_3(void *pv);
-
-//helper function
-void release_task_fun (TaskHandle_t handle, task_type input_type, uint32_t input_id, uint32_t input_deadline);
-void task_done_fun (uint32_t id);//send the id back when task is done 
+//three task handlers 
 
 
-dd_task_list  *get_scheduled_task_list (void);
-dd_task_list  *get_done_task_list (void);
-dd_task_list *get_task_missed_list
+//functions for our lists 
+void handle_release(message_dds *input_message);
+void handle_complete(message_dds *input_message);
+void handle_deadline_miss(void);
+void adjust_priorities(void);
+
+void insert_node(dd_task_list **head, dd_task task);//pass the head of the list and the task we want to put in 
+dd_task_list *remove_node(dd_task_list **head, uint32_t task_id);
+uint32_t list_size(dd_task_list  *head);
+
+//functions for scheduler 
+void release_dd_task (TaskHandle_t h, task_type t, uint32_t id, uint32_t deadline);
+void complete_dd_task(uint32_t input_id);
+dd_task_list *get_active_dd_task_list(void);
+dd_task_list *get_completed_dd_task_list(void);
+dd_task_list *get_overdue_dd_task_list(void);
+
+
+//timeout for ques
+static TickType_t que_timeout = portMAX_DELAY;
+
+///////////////////////////////////////////////////////////
+void insert_node(dd_task_list **head, dd_task task_to_add){
+    dd_task_list *new_node = pvPortMalloc(sizeof(dd_task_list));//create pointer and we request memory from the heap
+    new_node->task = task_to_add;
+    new_node->next = NULL;
+
+    //1) check if list is empty
+    if(*head== NULL){//list is empty
+        *head = new_node;//make the new node the head of the list 
+        return;
+    }
+    //check if task to add has the earliest deadline 
+    if(task_to_add.absolute_deadline <= (*head)->task.absolute_deadline){
+        new_node->next = *head; //new node point at head of list 
+        *head = new_node;//new node becomes start of the list with the lowest deadline
+        return;
+    }
+
+    dd_task_list *list_postion = *head;//pointer to parse list
+    //figure out where new node goes 
+    while(list_postion->next != NULL && list_postion->next->task.absolute_deadline <= task_to_add.absolute_deadline){
+        list_postion = list_postion->next;//next element of list 
+
+    }
+    new_node->next = list_postion->next; //new node tied to next of current postion
+    list_postion->next= new_node;//finishes adding node 
+}
+
+
+
+
+dd_task_list *remove_node(dd_task_list **head, uint32_t task_id_to_remove){
+    //list is empty return NULL
+    if(*head==NULL){
+        return NULL;
+    }
+
+    //is the node at the head
+    if((*head)->task.task_id == task_id_to_remove){//compare 
+        //task id is at the head of the list 
+        dd_task_list *removed = *head;//node for function to return
+        *head = (*head)->next;//point head of the list to the 2nd element
+        removed->next = NULL;//point the removed node 
+        return removed;//return the popped node 
+        //dont free memory because we will use same memory for the completed list 
+    }
+    //to remove a node from a list we need to store the node before and the after
+    dd_task_list *before = *head;
+    dd_task_list *after = (*head)->next;
+    while(after!= NULL){
+        if(after->task.task_id == task_id_to_remove){
+            before->next = after->next;
+            after->next  = NULL;
+            return after;
+        }
+        before =after;
+        after =after->next;
+        
+    }
+    //task was not found in the list
+    return NULL;
+}
+uint32_t list_size(dd_task_list *head){
+    dd_task_list *list_postion = head;
+    uint32_t c; //count
+    //parse the list 
+    while(list_postion !=NULL){
+        c++;//increment count
+        list_postion = list_postion->next;
+    }
+    return c;
+}
+///////////////////////////////////////////////////////////
+//*****setting priority
+//set head of list to highest priority everything else gets low priority 
+void adjust_priorities(void){
+    //check if its empty
+    if(active_task_list==NULL){
+        que_timeout= portMAX_DELAY;
+    }
+    //give head of list high priorirty
+    vTaskPrioritySet(active_task_list->task.t_handle, 3);
+    //parse list give all others low
+    dd_task_list *list_position = active_task_list->next;
+    while(list_position !=NULL){
+        //set lowest priority 
+        vTaskPrioritySet(list_position->task.t_handle, 1);
+        list_postion = list_postion->next;
+    }
+
+}
+
+
 
 
 
